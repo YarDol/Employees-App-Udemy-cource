4cdfb639f7ffd82cce6da70ed4769db293292d2b
Yar <slvwinswim@gmail.com>
2023-08-21 17:35:20 +0300

    layout design

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+       <div className="app-info">
+        <h1>Employee records N</h1>
+        <h2>Total number of staff: </h2>
+        <h2>Award: </h2>
+       </div>
+    )
+}
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,9 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text" className="form-control search-input" placeholder="Find employee"></input>
+    )
+}
+
+export default SearchPanel;
